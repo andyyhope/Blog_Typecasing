4cd2f74889de70b2c1390bad6c3276dc126adbe6
Andyy Hope <andyyhope@users.noreply.github.com>
2016-08-14 15:43:52 +1000

    UPDATE: Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Blog_Typecasing
-Enums as data models?
+![](https://cdn-images-1.medium.com/max/1600/1*XUa79Ukg_idcjzDkQNJ6wA.png)
+
+# Swift: Typecasing
+## Enums as data models?
+
+Every once in a while you come across one of those unicorn edge cases that force you to challenge everything you know about everything you’ve ever learnt up until that current point in time and space. Just recently I had become a victim of such an event.
+
+I had a chance to revisit, remodel and refactor an API response for one of the apps I work on where it would receive a JSON structure that had mixed models inside of an array. The reason why they’re mixed is because they need to be represented in a UITableView from within the same section, in a chronologial order. So this made receiving the data in two separate arrays and combining them afterwards a non-option.
+
+--
+
+tl;dr:
+Turn these
+
+```swift
+
+if models.characters[indexPath.row] is Hero {
+	// Do something
+}
+
+if let hero = models.characters[indexPath.row] as? Hero {
+	print(hero.power)
+}
+```
+
+Into this
+
+```swift
+switch model.characters[indexPath.row] {
+    case .hero(let hero):
+        print(hero.power)
+    
+    case .princess(let princess):
+        print(princess.kingdom)
+    
+    case .civilian(let civilian):
+        print(civilian.name)
+}
+```
---
+++ SwiftTypecasing.playground/Contents.swift
@@ -0,0 +1,117 @@
+
+// Swift: Typecasing
+// Enums as data models?
+//
+// Author: Andyy Hope
+//
+// Twitter: @andyyhope
+
+
+import Foundation
+
+
+// MARK: - Models
+
+struct Hero {
+    let name: String
+    let power: String
+    
+    init?(json: [String : AnyObject]) {
+        guard let
+            name = json["name"] as? String,
+            power = json["power"] as? String
+            else { return nil }
+        
+        self.name = name
+        self.power = power
+    }
+}
+
+struct Princess {
+    let name: String
+    let kingdom: String
+    
+    init?(json: [String : AnyObject]) {
+        guard let
+            name = json["name"] as? String,
+            kingdom = json["kingdom"] as? String
+            else { return nil }
+
+        self.name = name
+        self.kingdom = kingdom
+    }
+}
+
+struct Civilian {
+    let name: String
+    
+    init?(json: [String : AnyObject]) {
+        guard let name = json["name"] as? String else { return nil }
+
+        self.name = name
+    }
+}
+
+enum Character {
+    case hero(Hero)
+    case princess(Princess)
+    case civilian(Civilian)
+    
+    private enum Type : String {
+        case hero, princess, civilian
+        static let key = "type"
+    }
+    
+    init?(json: [String : AnyObject]) {
+        guard let
+            string = json[Type.key] as? String,
+            type = Type(rawValue: string)
+            else { return nil }
+        
+        switch type {
+        case .hero:
+            guard let hero = Hero(json: json) else { return nil }
+            self = .hero(hero)
+            
+        case .princess:
+            guard let princess = Princess(json: json) else { return nil }
+            self = .princess(princess)
+            
+        case .civilian:
+            guard let civilian = Civilian(json: json) else { return nil }
+            self = .civilian(civilian)
+        }
+    }
+}
+
+
+// MARK: - Typecasing
+
+let characters: [Character] = []
+
+characters.forEach { (character) in
+    
+    switch character {
+    case .hero(let hero):
+        print(hero.power)
+        
+    case .princess(let princess):
+        print(princess.kingdom)
+        
+    case .civilian(let civilian):
+        print(civilian.name)
+    }
+}
+
+
+// MARK: - Typecasing with Pattern Matching
+
+func printPower(character: Character) {
+    if case .hero(let hero) = character {
+        print(hero.power)
+    }
+}
+
+
+
+
